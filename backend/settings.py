1971ab758ecee19a9a28a428029390e5153a8e3a
Jordi <jordi8101@gmail.com>
2021-06-19 20:43:47 -0700

    cart enabled

--- backend/settings.py
+++ backend/settings.py
@@ -88,7 +88,7 @@ ROOT_URLCONF = "backend.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'frontend/build')],
+        "DIRS": [os.path.join(BASE_DIR, "frontend/build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -154,7 +154,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "frontend/build/static"]
+STATICFILES_DIRS = [BASE_DIR / "frontend/build/static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
